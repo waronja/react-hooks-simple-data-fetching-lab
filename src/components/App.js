41141a8ebcdc477fe782c34dc078360d459df1a4
simon waronja <simon.waronja@student.moringaschool.com>
2022-07-20 13:19:18 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  if (!message) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+        <img src={message} alt="A Random Dog" />
+    );
+  }
+}
+
+export default App;
